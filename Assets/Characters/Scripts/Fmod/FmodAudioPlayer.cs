fd3ca777cd88f5d33f5cb860f095dd00a54ff6d4
Abhishek Sonkar <sonkar.abhishek86@gmail.com>
2025-05-04 19:03:18 +0530

    Fmod audio player (WIP)

---
+++ Assets/Characters/Scripts/Fmod/FmodAudioPlayer.cs
@@ -0,0 +1,15 @@
+using FMODUnity;
+using UnityEngine;
+
+public class FmodAudioPlayer : MonoBehaviour
+{
+    public void PlayOneShot(EventReference eventRef)
+    {
+        RuntimeManager.PlayOneShot(eventRef);
+    }
+
+    public void PlayOneShot(string eventPath)
+    {
+        RuntimeManager.PlayOneShot(eventPath);
+    }
+}
