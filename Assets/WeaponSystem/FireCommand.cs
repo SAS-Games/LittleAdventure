b89acd3654dcbfc7482ecdd2c9d4fc0e4c03affc
Abhishek Sonkar <sonkar.abhishek86@gmail.com>
2025-07-19 14:57:55 +0530

    fixed fire command

--- Assets/WeaponSystem/FireCommand.cs
+++ Assets/WeaponSystem/FireCommand.cs
@@ -1,63 +1,26 @@
 ﻿using SAS.StateMachineCharacterController;
-using System;
 using UnityEngine.InputSystem;
-using static UnityEngine.InputSystem.InputAction;
 
 namespace SAS.WeaponSystem
 {
-    public class FireCommand : IInputCommand
+    public class FireCommand : ChainedInputCommand
     {
-        private Action<CallbackContext> _firePerformed;
-        private Action<CallbackContext> _fireCanceled;
-        private InputAction _inputAction;
-        private string _inputActionKey;
+        protected override string InputActionName { get; }
 
         public FireCommand(string inputActionKey, Weapon weapon, FSMCharacterController fsmController)
         {
-            _inputActionKey = inputActionKey;
-            _firePerformed = _ =>
+            InputActionName = inputActionKey;
+            AddHandler(InputActionPhase.Performed, new ConditionalInputHandler(() => true, _ =>
             {
                 weapon.CurrentInput = true;
                 fsmController.OnFire();
-            };
-            _fireCanceled = _ =>
+            }));
+
+            AddHandler(InputActionPhase.Canceled, new ConditionalInputHandler(() => true, _ =>
             {
                 weapon.CurrentInput = false;
                 fsmController.OnFireCanceled();
-            };
-        }
-
-        public void AddFirePerformedCallback(Action<CallbackContext> callback) => _firePerformed += callback;
-        public void RemoveFirePerformedCallback(Action<CallbackContext> callback) => _firePerformed -= callback;
-        public void AddFireCanceledCallback(Action<CallbackContext> callback) => _fireCanceled += callback;
-        public void RemoveFireCanceledCallback(Action<CallbackContext> callback) => _fireCanceled -= callback;
-
-        public void SetActive(InputConfig inputConfig, bool active)
-        {
-            if (active)
-            {
-                if (_inputAction != null)
-                {
-                    _inputAction.performed -= _firePerformed;
-                    _inputAction.canceled -= _fireCanceled;
-                    _inputAction.Disable();
-                }
-
-                _inputAction = inputConfig.GetInputAction(_inputActionKey);
-                if (_inputAction != null)
-                {
-                    _inputAction.performed += _firePerformed;
-                    _inputAction.canceled += _fireCanceled;
-                    _inputAction.Enable();
-                }
-            }
-            else if (_inputAction != null)
-            {
-                _inputAction.performed -= _firePerformed;
-                _inputAction.canceled -= _fireCanceled;
-                _inputAction.Disable();
-                _inputAction = null;
-            }
+            }));
         }
     }
 }
