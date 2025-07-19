adda3fe10881eed500d890fc8baafec4696b59a3
Abhishek Sonkar <sonkar.abhishek86@gmail.com>
2025-07-19 14:55:32 +0530

    fixed Attack dash

--- Assets/Characters/Player/Scripts/AttackDashHandler.cs
+++ Assets/Characters/Player/Scripts/AttackDashHandler.cs
@@ -2,6 +2,7 @@ using System;
 using SAS.StateMachineCharacterController;
 using SAS.StateMachineGraph;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class AttackDashHandler : MonoBehaviour
 {
@@ -9,24 +10,24 @@ public class AttackDashHandler : MonoBehaviour
     [SerializeField] private string m_AttackStateName = "Attack";
     [SerializeField] private string m_DashInputName = "Dash";
     private Actor _actor;
+    private IConditionalInputHandler _handler;
 
     void Start()
     {
         _actor = GetComponentInParent<Actor>();
-        var inputCommand = GetComponentInParent<InputHandler>().GetCommand(m_DashInputName);
-        (inputCommand as IInputCallbackRegistry).RegisterCallback(DashCallback);
+        var dashInputCommand = GetComponentInParent<InputHandler>().GetCommand(m_DashInputName);
+        _handler = new ConditionalInputHandler(() => _actor.CurrentStateName == m_AttackStateName, DashCallback);
+        dashInputCommand.AddHandler(InputActionPhase.Performed, _handler, 1);
     }
 
-    private void DashCallback()
+    private void DashCallback(InputAction.CallbackContext context)
     {
-        if (_actor.CurrentStateName == m_AttackStateName)
-            _actor.SetState(m_DashStateName);
+        _actor.SetState(m_DashStateName);
     }
 
     private void OnDestroy()
     {
         var inputCommand = GetComponentInParent<InputHandler>()?.GetCommand(m_DashInputName);
-        if (inputCommand != null)
-            (inputCommand as IInputCallbackRegistry).UnregisterCallback(DashCallback);
+        inputCommand?.RemoveHandler(InputActionPhase.Performed, _handler);
     }
 }
