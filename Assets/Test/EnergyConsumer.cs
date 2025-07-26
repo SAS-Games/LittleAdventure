f477433c26b73632bb4003dbf3286d6e1bc55a99
Abhishek Sonkar <sonkar.abhishek86@gmail.com>
2025-07-26 23:06:40 +0530

    energy consume state

---
+++ Assets/Test/EnergyConsumer.cs
@@ -0,0 +1,24 @@
+using SAS.StateMachineCharacterController;
+using SAS.StateMachineGraph;
+using SAS.Utilities.BlackboardSystem;
+using SAS.Utilities.TagSystem;
+using UnityEngine;
+
+public class EnergyConsumer : IStateAction
+{
+    [Inject] protected IEnergyModel _energyModel;
+
+    [SerializeField] private float _energyCost;
+    private BlackboardKey _energyCostKey;
+
+    void IStateAction.OnInitialize(Actor actor, Tag tag, string key)
+    {
+        actor.Initialize(this);
+        actor.TryGet<float>(new BlackboardKey(FSMCharacterBlackboardKey.EnergyCost), out _energyCost);
+    }
+
+    void IStateAction.Execute(ActionExecuteEvent executeEvent)
+    {
+        _energyModel.Decrease(_energyCost);
+    }
+}
