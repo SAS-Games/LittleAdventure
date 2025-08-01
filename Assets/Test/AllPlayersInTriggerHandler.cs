37b2732e683521957334c75ad18c42668107a0cf
Abhi86shek <sonkar.abhishek86@gmail.com>
2025-08-01 14:13:47 +0530

    code refactoring

---
+++ Assets/Test/AllPlayersInTriggerHandler.cs
@@ -0,0 +1,51 @@
+using SAS.Utilities.TagSystem;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(OnTriggerHandler), typeof(IEntityPresenceProvider))]
+public abstract class AllPlayersInTriggerHandler : MonoBehaviour
+{
+    [FieldRequiresSelf] protected IEntityPresenceProvider _presenceProvider;
+    protected readonly HashSet<GameObject> _playersInside = new();
+    private bool _actionTriggered = false;
+
+    private void Awake()
+    {
+        this.Initialize();
+    }
+
+    public void NotifyPlayerEntered(GameObject player)
+    {
+        if (!GetPresentEntities().Contains(player)) return;
+
+        _playersInside.Add(player);
+        CheckAllPlayersInside();
+    }
+
+    public void NotifyPlayerExited(GameObject player)
+    {
+        _playersInside.Remove(player);
+    }
+
+    private void CheckAllPlayersInside()
+    {
+        if (_actionTriggered) return;
+
+        var activePlayers = GetPresentEntities();
+        if (activePlayers.Count == 0) return;
+
+        if (activePlayers.All(p => _playersInside.Contains(p)))
+        {
+            _actionTriggered = true;
+            OnAllPlayersInside();
+        }
+    }
+
+    protected abstract void OnAllPlayersInside();
+
+    private IReadOnlyList<GameObject> GetPresentEntities()
+    {
+        return _presenceProvider.GetPresentEntities() ?? new List<GameObject>();
+    }
+}
--- Assets/Test/SceneGroupTrigger.cs
+++ Assets/Test/SceneGroupTrigger.cs
@@ -1,49 +1,10 @@
-using SAS.Utilities.TagSystem;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
-[RequireComponent(typeof(SceneGroupLoader), typeof(OnTriggerHandler), typeof(IEntityPresenceProvider))]
-public class SceneGroupTrigger : MonoBehaviour
+[RequireComponent(typeof(SceneGroupLoader))]
+public class SceneGroupTrigger : AllPlayersInTriggerHandler
 {
-    [FieldRequiresSelf] IEntityPresenceProvider _activePlayerProvider;
-    private readonly HashSet<GameObject> _playersInside = new();
-    private bool _sceneLoadingTriggered = false;
-
-    private void Awake()
+    protected override void OnAllPlayersInside()
     {
-        this.Initialize();
-    }
-
-    public void NotifyPlayerEntered(GameObject player)
-    {
-        if (!GetPresentEntities().Contains(player)) return;
-
-        _playersInside.Add(player);
-        CheckAllPlayersInside();
-    }
-
-    public void NotifyPlayerExited(GameObject player)
-    {
-        _playersInside.Remove(player);
-    }
-
-    private void CheckAllPlayersInside()
-    {
-        if (_sceneLoadingTriggered) return;
-
-        var activePlayers = GetPresentEntities();
-        if (activePlayers.Count == 0) return;
-
-        if (activePlayers.All(p => _playersInside.Contains(p)))
-        {
-            _sceneLoadingTriggered = true;
-            GetComponent<SceneGroupLoader>().Load();
-        }
-    }
-
-    private IReadOnlyList<GameObject> GetPresentEntities()
-    {
-        return _activePlayerProvider.GetPresentEntities() ?? new List<GameObject>();
+        GetComponent<SceneGroupLoader>().Load();
     }
 }
