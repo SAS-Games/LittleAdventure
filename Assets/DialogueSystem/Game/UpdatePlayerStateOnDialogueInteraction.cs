c0982921adda4f705e27d740974402d4c842e6bb
Abhi86shek <sonkar.abhishek86@gmail.com>
2025-08-06 22:46:43 +0530

    more dlg system. game specific

---
+++ Assets/DialogueSystem/Game/UpdatePlayerStateOnDialogueInteraction.cs
@@ -0,0 +1,20 @@
+using SAS.StateMachineGraph.Utilities;
+using UnityEngine;
+
+public class UpdatePlayerStateOnDialogueInteraction : EntityInteractionHandler
+{
+    [SerializeField] protected Parameter m_InteractParamOn;
+    [SerializeField] protected Parameter m_InteractParamOff;
+
+    public void OnDialogueStart()
+    {
+        var playersInBounds = GetEntitiesWithinBounds();
+        MoveEntitiesToMarkers(playersInBounds);
+        ApplyParameterToEntities(_presenceProvider.GetPresentEntities(), m_InteractParamOn);
+    }
+
+    public void OnDialogueEnd()
+    {
+        ApplyParameterToEntities(_presenceProvider.GetPresentEntities(), m_InteractParamOff);
+    }
+}
---
+++ Assets/Test/EntityInteractionHandler.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using System.Collections.Generic;
+using SAS.Utilities.TagSystem;
+using SAS.StateMachineCharacterController;
+using SAS.StateMachineGraph;
+using SAS.StateMachineGraph.Utilities;
+
+public abstract class EntityInteractionHandler : MonoBehaviour
+{
+    [SerializeField] protected Transform[] m_Markers;
+    [FieldRequiresSelf] protected IEntityPresenceProvider _presenceProvider;
+
+    protected Collider _proximityCollider;
+
+    protected virtual void Awake()
+    {
+        this.Initialize();
+        _proximityCollider = GetComponent<Collider>();
+    }
+
+    protected List<GameObject> GetEntitiesWithinBounds()
+    {
+        var players = _presenceProvider.GetPresentEntities();
+        var result = new List<GameObject>();
+
+        foreach (var entity in players)
+        {
+            if (_proximityCollider.bounds.Contains(entity.transform.position))
+                result.Add(entity);
+        }
+
+        return result;
+    }
+
+    protected void ApplyParameterToEntities(IEnumerable<GameObject> entities, Parameter actorParam)
+    {
+        foreach (var obj in entities)
+        {
+            var actor = obj.GetComponent<Actor>();
+            actor?.Apply(in actorParam);
+        }
+    }
+
+    protected void MoveEntitiesToMarkers(List<GameObject> entities)
+    {
+        for (int i = 0; i < entities.Count && i < m_Markers.Length; i++)
+        {
+            var obj = entities[i];
+            var controller = obj.GetComponent<FSMCharacterController>();
+
+            if (controller != null)
+            {
+                controller.SetPosition(m_Markers[i].position);
+                obj.transform.rotation = m_Markers[i].rotation;
+            }
+        }
+    }
+}
