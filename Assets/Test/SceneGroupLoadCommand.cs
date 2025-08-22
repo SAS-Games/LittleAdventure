12396d27e5ff99df6a4d4b8e6063809c16bae73f
Abhishek Sonkar <sonkar.abhishek86@gmail.com>
2025-08-23 00:24:55 +0530

    scene group loader command

---
+++ Assets/Test/SceneGroupLoadCommand.cs
@@ -0,0 +1,32 @@
+﻿using SAS.SceneManagement;
+using SAS.Utilities.TagSystem;
+using UnityEngine;
+
+namespace SAS.Utilities.DeveloperConsole
+{
+    [CreateAssetMenu(fileName = "New SceneGroupLoadCommand", menuName = "LittleAdventure/DeveloperConsole/Commands/SceneGroupLoadCommand")]
+    public class SceneGroupLoadCommand : ConsoleCommand
+    {
+        [Inject] private ISceneLoader _sceneLoader;
+        public override string HelpText => "Usage: LoadSceneGroup [group name]. Load the scene group with the given group name.";
+
+        public override bool Process(DeveloperConsoleBehaviour developerConsole, string command, string[] args)
+        {
+            if (args != null && args.Length > 0)
+            {
+                bool loadOptionalScenes = false;
+                if (args.Length > 1)
+                {
+                    if (!string.IsNullOrEmpty(args[1]) && !bool.TryParse(args[1], out loadOptionalScenes))
+                        return false;
+                }
+                if(_sceneLoader == null)
+                    developerConsole.InjectFieldBindings(this);
+                _sceneLoader.LoadSceneGroupAsync(args[0], loadOptionalScenes);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
