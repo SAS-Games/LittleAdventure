459f6a0af50252dd8e395dc19e30371d74306285
Abhishek Sonkar <sonkar.abhishek86@gmail.com>
2026-09-22 19:59:14 +0530

    removed local package

--- Packages/com.sas.dialogue-system/Runtime/Dialogue/IDialogueAnimationTarget.cs
+++
@@ -1,8 +0,0 @@
-/// <summary>
-/// Optional bridge for forwarding dialogue portrait animation states to another system.
-/// Implement this on the same GameObject as <see cref="SpeakerView"/>.
-/// </summary>
-public interface IDialogueAnimationTarget
-{
-    void Process(string stateName);
-}
